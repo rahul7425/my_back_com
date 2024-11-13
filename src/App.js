8b5af2ead59c761b36085a595d55d00c312aa53c
Nandini <singhnandini9358@gmail.com>
2024-11-13 12:21:05 +0530

    push home or about file changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import home from './component/home';
+import about from './component/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Link to=''>Home</Link>
+        <Link to='/about'>about</Link>
+        <Routes>
+        <Route path='' Component={home} />
+        <Route path='/about' Component={about} />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
